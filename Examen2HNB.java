48061861e0910ee4076f7bc719f9aa0a882a6d28
HashemNAVA77 <143622962+HashemNAVA77@users.noreply.github.com>
2023-09-20 08:56:25 -0600

    Add files via upload

---
+++ Examen2HNB.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examen2.hnb;
+//import java.util.Scanner;Scanner entrada.
+
+/**
+ *
+ * @author Usuario
+ */
+public class Examen2HNB {
+ 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic 
+      System.out.print("Solicitar nombre de instrumento");
+      
+      System.out.print("Precio de compra");
+       
+        double instruemnto1 = (1250.45);
+        double instrumento2 = (3743.00);
+        double instruemto3 = (2683.78);
+        
+        
+    }   
+    
+    
+   
+}
+    
+ 
