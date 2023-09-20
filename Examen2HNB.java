7f9693f041debedcff3541d5c73e05e199c9e0a7
HashemNAVA77 <143622962+HashemNAVA77@users.noreply.github.com>
2023-09-20 11:28:10 -0600

    Update Examen2HNB.java

--- Examen2HNB.java
+++ Examen2HNB.java
@@ -14,15 +14,54 @@ public class Examen2HNB {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic 
-      System.out.print("Solicitar nombre de instrumento");
-      
-      System.out.print("Precio de compra");
-       
-        double instruemnto1 = (1250.45);
-        double instrumento2 = (3743.00);
-        double instruemto3 = (2683.78);
+    
+      public static void main(String[] args) {
+      // TODO code application logic here double guitarra = 1250.45;
+
+double piano = 3743.00;
+
+double bajo = 2683.78;
+
+Scanner lectura = new Scanner(System.in);
+
+System.out.println("Nombre del instrumento");
+ String nombret = lectura.nextLine():
+
+System.out.println("Nombre del instrumento"); 
+ String nombre2 = lectura.nextLine();
+
+System.out.println("Nombre del instrumento");
+
+String nombre3 = lectura.nextLine();
+
+System.out.println("Ingrese el precio de compra"):
+
+double precioCompra1 = lectura.nextDouble();
+System.out.println("Ingrese el precio de compra"); 
+double precioCompra2 = lectura.nextDouble();
+System.out.println("Ingrese el precio de compra");
+
+double precioCompra3 = lectura.nextDouble();
+
+double ganacial = guitarra - precioCompral;
+double ganacia2 = piano - precioCompra2;
+
+double ganacia3 bajo precioCompra3;
+
+double total1 = (ganacial/precioCompra1)*100;
+
+double total2 = (ganacia2/precioCompra2)*100;
+
+double total3 (ganacia3/precioCompra3)*100;
+
+System.out.println("Ganacia de la guitarra es: ganacia1);
+
+System.out.println("Ganacia del piano es: " + ganacia2);
+System.out.println("Ganacia del bajo es: " + ganacia3);
+
+System.out.println("Total de la guitarra es: " + total1); 
+System.out.println("Total del piano es: " + total2); 
+System.out.println("Total del bajo es: total3):
         
         
     }   
